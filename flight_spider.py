292895953ab6dc2c94d1d1ea4818c9fe597e30e5
chenwenbin69 <1369168527@qq.com>
2020-03-29 19:57:49 +0800

    抓取携程飞机票价

---
+++ flight_spider.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8 -*-
+
+__author__ = 'wenbin'
+
+"""
+携程低价机票网址
+https://flights.ctrip.com/itinerary/roundtrip/sha-hld?date=2020-05-09%2C2020-05-12&sortByPrice=true
+"""
+
+import requests
+import json
+from datetime import datetime,timedelta
+
+city_dict = {'上海' : 'sha',
+             '北京' : 'bjs',
+             '呼伦贝尔' : 'hld'
+}
+source = city_dict['上海']
+destination = city_dict['呼伦贝尔']
+weeks = [0,1,2,3,4,5,6] #判断日期为周几时使用
+days = []
+duration = 2 #游玩周期3天
+# duration_plus = duration + 1
+url = 'https://flights.ctrip.com/itinerary/api/12808/lowestPrice'
+headers = {
+    'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
+}
+
+payload = {"flightWay":"Roundtrip","dcity":source,"acity":destination,"army":False}
+res = requests.post(url=url,json=payload)
+res = json.loads(res.text)
+# print(res['data']['roundTripPrice'])
+
+# 遍历所有旅程开始日期
+for k in res['data']['roundTripPrice'].keys():
+    days.append(k)
+
+for firstday in days:
+    week = datetime.strptime(firstday,"%Y%m%d").weekday()
+    if week == weeks[4]: #4是周五，预计周五出发，好请假
+        firstday_date = datetime.strptime(firstday,'%Y%m%d').date()
+        lastday_date = firstday_date + timedelta(days=duration)
+        print("出发地{}，目的地{}".format(source,destination))
+        print("出发日期：{}，返程日期：{}。".format(firstday_date,lastday_date))
+        lastday = datetime.strftime(lastday_date,'%Y%m%d')
+        price_list = res['data']['roundTripPrice'][firstday]
+        if lastday in price_list.keys():
+            print("间隔{}天往返总价：￥{}".format(duration,price_list[lastday]) + '\n')
+        else:
+            print("懒得处理了！" + '\n')
+
